8b31cdc8b143bea6ff12c945808535c4b94aec1b
SatejSawant45 <satejsawant90@gmail.com>
2024-12-23 23:01:00 +0530

    --

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,14 +1,33 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer , WebSocket } from "ws";
  
 const wss = new WebSocketServer({ port:8080 });
+
+let senderSocket: null | WebSocket= null;
+
+let recieverSocket:any = null;
 
 wss.on('connection',function connection(ws){
     ws.on('errror',console.error);
 
     ws.on('message',function message(data:any){
             const message = JSON.parse(data);
-            console.log(message);
+
+            if(message.type === "identify-as-sender")
+            {
+                senderSocket = ws;
+            }
+            else if(message.type === "identify-as-reciever")
+            {
+                recieverSocket = ws;
+            }
+            else if(message.type === "create-offer")
+            {
+                recieverSocket.send(JSON.stringify({type:"offer", offer:message.offer}))
+            }
+
+
+            
     });
 
-    ws.send("something");
+    
 });
